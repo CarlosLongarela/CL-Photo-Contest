d9f46cc69dc4d673f8e15a56787b0d5de77de1ed
Carlos Longarela <carlos@longarela.eu>
2018-11-17 17:43:43 +0100

    second table for photos creation

--- includes/class-cl-photo-contest-activator.php
+++ includes/class-cl-photo-contest-activator.php
@@ -35,29 +35,35 @@ class Cl_Photo_Contest_Activator {
 			wp_mkdir_p( CL_PHOTO_CONTEST_UPLOAD_PATH );
 		}
 
+		$sql = array();
+
 		// If db table doesen't exists, we'll create it.
-		// Our table name.
-		$table_name = $wpdb->prefix . 'cl_photo_contests';
+		// Our table names.
+		$table_name_1 = $wpdb->prefix . 'cl_photo_contests';
+		$table_name_2 = $wpdb->prefix . 'cl_photo_contests_photos';
 
 		// This is rather picky see https://codex.wordpress.org/Creating_Tables_with_Plugins for spaces, syntax...
 		$charset_collate = $wpdb->get_charset_collate();
 
-		$sql = 'CREATE TABLE ' . $table_name . " (
+		$sql[] = 'CREATE TABLE ' . $table_name_1 . " (
 		id mediumint(9) UNSIGNED NOT NULL AUTO_INCREMENT,
-		date datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-		mail varchar(255),
-		post_data text NOT NULL,
-		total_questions smallint(3) UNSIGNED DEFAULT '0',
-		empatia decimal(5,2) UNSIGNED DEFAULT '0',
-		resiliencia decimal(5,2) UNSIGNED DEFAULT '0',
-		integridad decimal(5,2) UNSIGNED DEFAULT '0',
-		responsabilidad decimal(5,2) UNSIGNED DEFAULT '0',
-		ejemplaridad decimal(5,2) UNSIGNED DEFAULT '0',
-		tolerancia decimal(5,2) UNSIGNED DEFAULT '0',
-		autoconocimiento decimal(5,2) UNSIGNED DEFAULT '0',
-		paciencia decimal(5,2) UNSIGNED DEFAULT '0',
-		curiosidad decimal(5,2) UNSIGNED DEFAULT '0',
-		esperanza decimal(5,2) UNSIGNED DEFAULT '0',
+		creation_date datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+		title varchar(255),
+		active_from datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+		active_to datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+		PRIMARY KEY  (id)
+		) " . $charset_collate . ';';
+
+		$sql[] = 'CREATE TABLE ' . $table_name_2 . " (
+		id mediumint(9) UNSIGNED NOT NULL AUTO_INCREMENT,
+		author_name varchar(255) NULL DEFAULT NULL,
+		author_mail varchar(255) NULL DEFAULT NULL,
+		photo_title varchar(255) NULL DEFAULT NULL,
+		photo_size_bytes mediumint(9) UNSIGNED NULL DEFAULT NULL,
+		photo_comment text NULL,
+		upload_date datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+		id_contest mediumint(9) UNSIGNED NULL DEFAULT NULL,
+		photo_validated tinyint(1) UNSIGNED NULL DEFAULT '0',
 		PRIMARY KEY  (id)
 		) " . $charset_collate . ';';
 
